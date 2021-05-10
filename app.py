0ac8a2cc9164b205ce812ae93e1559d808e3fbb2
tbob3211 <tbob3211@gmail.com>
2021-05-10 22:56:48 +1000

    Big Update/ Added app.py

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home(): 
+    return render_template('Theme.html')
+
+db={'tbob3211': '22629772', 'chenze': '23148505'}
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form["username"] not in db:
+            error = "access denied, username not found"
+        else:
+            if db[request.form["username"]] != request.form["password"]:
+                error = "access denied, password not found" 
+            else: 
+                return redirect(url_for('home'))
+
+    return render_template("login.html", error = error)
+
+@app.route('/arancini')
+def arancini(): 
+    return render_template('Minestrone_Soup.html')
+
+if __name__=="__main__":
+    app.run(debug=True)
